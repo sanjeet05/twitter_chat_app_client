bca23952527369c4fed24f4d0b932a2d331a678a
Sanjeet <sanjeet.kumar@zylotech.com>
2018-08-26 18:27:48 +0530

    wip

--- app/constants/ServerUrl.js
+++ app/constants/ServerUrl.js
@@ -2,11 +2,9 @@ let baseURL = '';
 let socketURL = '';
 
 if (process.env.NODE_ENV === 'production' ) {
-  // console.log('Production...');
-  baseURL = 'http://localhost:3100/api';
-  socketURL = 'http://localhost:3100';
-  // baseURL = 'https://nodeapp-v3.herokuapp.com/api';
-  // socketURL = 'https://nodeapp-v3.herokuapp.com';
+  // console.log('Production...');  
+  baseURL = 'https://twitter-chat-app-server.herokuapp.com/api';
+  socketURL = 'https://twitter-chat-app-server.herokuapp.com';
 } else {
   // console.log('Devlopment...');
   baseURL = 'http://localhost:3100/api';
